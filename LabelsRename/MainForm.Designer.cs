d91b204ba9fe3f48972e039a07f0a21d217ada78
Mohamed Ali <39552203+MohamedA-Ibrahim@users.noreply.github.com>
2023-01-08 13:55:59 +0200

    Add support for user defined renaming rules

--- LabelsRename/MainForm.Designer.cs
+++ LabelsRename/MainForm.Designer.cs
@@ -31,26 +31,30 @@ namespace LabelsRename
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.btnRemoveSelected = new System.Windows.Forms.Button();
+            this.ckRemoveDuplicates = new System.Windows.Forms.CheckBox();
+            this.lbHorizontalLine = new System.Windows.Forms.Label();
+            this.rtRenames = new System.Windows.Forms.RichTextBox();
+            this.btnClearList = new System.Windows.Forms.Button();
             this.btnRename = new System.Windows.Forms.Button();
             this.btnSelectTxtFile = new System.Windows.Forms.Button();
             this.lstLabelTracks = new System.Windows.Forms.ListBox();
-            this.lblTracksCount = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.lbHorizontalLine = new System.Windows.Forms.Label();
+            this.lblLineCount = new System.Windows.Forms.Label();
+            this.lblTracksCount = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.ckRemoveDuplicates);
             this.groupBox2.Controls.Add(this.lbHorizontalLine);
-            this.groupBox2.Controls.Add(this.richTextBox1);
-            this.groupBox2.Controls.Add(this.btnRemoveSelected);
+            this.groupBox2.Controls.Add(this.rtRenames);
+            this.groupBox2.Controls.Add(this.btnClearList);
             this.groupBox2.Controls.Add(this.btnRename);
             this.groupBox2.Controls.Add(this.btnSelectTxtFile);
             this.groupBox2.Controls.Add(this.lstLabelTracks);
             this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.lblLineCount);
             this.groupBox2.Controls.Add(this.lblTracksCount);
             this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(12, 12);
@@ -60,17 +64,49 @@ namespace LabelsRename
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Rename labels in label tracks";
             // 
-            // btnRemoveSelected
+            // ckRemoveDuplicates
             // 
-            this.btnRemoveSelected.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemoveSelected.ForeColor = System.Drawing.Color.Black;
-            this.btnRemoveSelected.Location = new System.Drawing.Point(349, 294);
-            this.btnRemoveSelected.Name = "btnRemoveSelected";
-            this.btnRemoveSelected.Size = new System.Drawing.Size(192, 37);
-            this.btnRemoveSelected.TabIndex = 1;
-            this.btnRemoveSelected.Text = "Remove Selected File";
-            this.btnRemoveSelected.UseVisualStyleBackColor = true;
-            this.btnRemoveSelected.Click += new System.EventHandler(this.btnRemoveSelected_Click);
+            this.ckRemoveDuplicates.AutoSize = true;
+            this.ckRemoveDuplicates.Checked = true;
+            this.ckRemoveDuplicates.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckRemoveDuplicates.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckRemoveDuplicates.Location = new System.Drawing.Point(351, 324);
+            this.ckRemoveDuplicates.Name = "ckRemoveDuplicates";
+            this.ckRemoveDuplicates.Size = new System.Drawing.Size(196, 23);
+            this.ckRemoveDuplicates.TabIndex = 8;
+            this.ckRemoveDuplicates.Text = "Remove Duplicate Lines";
+            this.ckRemoveDuplicates.UseVisualStyleBackColor = true;
+            // 
+            // lbHorizontalLine
+            // 
+            this.lbHorizontalLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbHorizontalLine.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHorizontalLine.Location = new System.Drawing.Point(6, 194);
+            this.lbHorizontalLine.Name = "lbHorizontalLine";
+            this.lbHorizontalLine.Size = new System.Drawing.Size(531, 2);
+            this.lbHorizontalLine.TabIndex = 6;
+            // 
+            // rtRenames
+            // 
+            this.rtRenames.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtRenames.Location = new System.Drawing.Point(6, 48);
+            this.rtRenames.Name = "rtRenames";
+            this.rtRenames.Size = new System.Drawing.Size(531, 121);
+            this.rtRenames.TabIndex = 4;
+            this.rtRenames.Text = "";
+            this.rtRenames.TextChanged += new System.EventHandler(this.rtRenameRules_TextChanged);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearList.ForeColor = System.Drawing.Color.Black;
+            this.btnClearList.Location = new System.Drawing.Point(349, 269);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(192, 37);
+            this.btnClearList.TabIndex = 1;
+            this.btnClearList.Text = "Clear List";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
             // 
             // btnRename
             // 
@@ -107,27 +143,6 @@ namespace LabelsRename
             this.lstLabelTracks.Size = new System.Drawing.Size(333, 196);
             this.lstLabelTracks.TabIndex = 3;
             // 
-            // lblTracksCount
-            // 
-            this.lblTracksCount.AutoSize = true;
-            this.lblTracksCount.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTracksCount.ForeColor = System.Drawing.Color.Black;
-            this.lblTracksCount.Location = new System.Drawing.Point(352, 261);
-            this.lblTracksCount.Name = "lblTracksCount";
-            this.lblTracksCount.Size = new System.Drawing.Size(57, 19);
-            this.lblTracksCount.TabIndex = 0;
-            this.lblTracksCount.Text = "Count:";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(6, 48);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(531, 134);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -135,18 +150,31 @@ namespace LabelsRename
             this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.label1.Location = new System.Drawing.Point(6, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(297, 19);
+            this.label1.Size = new System.Drawing.Size(379, 19);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Current replacements being made :";
+            this.label1.Text = "Write the names you want replaced: old|new";
             // 
-            // lbHorizontalLine
+            // lblLineCount
             // 
-            this.lbHorizontalLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lbHorizontalLine.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbHorizontalLine.Location = new System.Drawing.Point(6, 194);
-            this.lbHorizontalLine.Name = "lbHorizontalLine";
-            this.lbHorizontalLine.Size = new System.Drawing.Size(531, 2);
-            this.lbHorizontalLine.TabIndex = 6;
+            this.lblLineCount.AutoSize = true;
+            this.lblLineCount.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLineCount.ForeColor = System.Drawing.Color.Black;
+            this.lblLineCount.Location = new System.Drawing.Point(514, 172);
+            this.lblLineCount.Name = "lblLineCount";
+            this.lblLineCount.Size = new System.Drawing.Size(18, 19);
+            this.lblLineCount.TabIndex = 0;
+            this.lblLineCount.Text = "0";
+            // 
+            // lblTracksCount
+            // 
+            this.lblTracksCount.AutoSize = true;
+            this.lblTracksCount.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTracksCount.ForeColor = System.Drawing.Color.Black;
+            this.lblTracksCount.Location = new System.Drawing.Point(352, 246);
+            this.lblTracksCount.Name = "lblTracksCount";
+            this.lblTracksCount.Size = new System.Drawing.Size(57, 19);
+            this.lblTracksCount.TabIndex = 0;
+            this.lblTracksCount.Text = "Count:";
             // 
             // MainForm
             // 
@@ -171,10 +199,12 @@ namespace LabelsRename
         private System.Windows.Forms.Button btnSelectTxtFile;
         private System.Windows.Forms.ListBox lstLabelTracks;
         private System.Windows.Forms.Label lblTracksCount;
-        private System.Windows.Forms.Button btnRemoveSelected;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.RichTextBox rtRenames;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbHorizontalLine;
+        private System.Windows.Forms.Label lblLineCount;
+        private System.Windows.Forms.CheckBox ckRemoveDuplicates;
     }
 }
 
--- LabelsRename/MainForm.cs
+++ LabelsRename/MainForm.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace LabelsRename
 {
     public partial class MainForm : Form
     {
+        Dictionary<string, string> replacements;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            lstLabelTracks.Items.Clear();
+        }
+
+        private void rtRenameRules_TextChanged(object sender, EventArgs e)
+        {
+            var lineCount = rtRenames.Lines.Count();
+            lblLineCount.Text = lineCount.ToString();
         }
 
 
@@ -34,7 +43,7 @@ namespace LabelsRename
                 //If the user selects a file proceed
                 if (ofdFile.ShowDialog() == DialogResult.OK)
                 {
-                    foreach (String file in ofdFile.FileNames)
+                    foreach (string file in ofdFile.FileNames)
                     {
                         if (!lstLabelTracks.Items.Contains(file))
                         {
@@ -46,54 +55,86 @@ namespace LabelsRename
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
         }
 
-        private void RenameLabels(string fileLocation)
-        {
-            var oldLines = File.ReadLines(fileLocation);
-
-            var newLines = oldLines.Select(s => 
-            s.Replace("MorroDefau_MDQGreeting_", "MorroDefau_MDQGreetingTopi_")
-            .Replace("MorroDefau_MDQGreetings_", "MorroDefau_MDQGreetingsTop_")
-            .Replace("MorroDefaultQuest_MDQIdle_", "MorroDefau_MDQIdleLinesTop_")
-            .Replace("MorroDefaultQuest_MDQTopic_", "MorroDefau_MDQMolagMarTopi_")
-            .Replace("MorroDefau_MDQTopic_", "MorroDefau_MDQMolagMarTopi_")
-            .Replace("MorroDefau_MDQVivecTopicCi_", "MorroDefau_MDQVivecTopic_")
-            .Trim())
-                .ToList();
-
-            File.WriteAllLines(fileLocation, newLines);
-        }
 
         private void btnRename_Click(object sender, EventArgs e)
         {
+
+            if (lstLabelTracks.Items.Count == 0)
+                return;
+
+            ReadReplacementRules();
+
+            foreach (string file in lstLabelTracks.Items)
+            {
+                RenameLabels(file);
+
+                if(ckRemoveDuplicates.Checked)
+                    RemoveDuplicates(file);
+            }
+
+            MessageBox.Show($"Renamed {lstLabelTracks.Items.Count} File(s) Successfully");
+        }
+
+
+        /// <summary>
+        /// Read the textbox and add their values to the dictionary
+        /// </summary>
+        private void ReadReplacementRules()
+        {
             try
             {
-
-                if (lstLabelTracks.Items.Count != 0)
+                replacements = new Dictionary<string, string>();
+                foreach (string str in rtRenames.Lines)
                 {
-                    foreach (string file in lstLabelTracks.Items)
-                    {
-                        RenameLabels(file);
-                    }
-                    MessageBox.Show("Success");
+                    replacements.Add(str.Split('|')[0], str.Split('|')[1]);
                 }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message, "Make sure you wrote your replacements correctly", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        private void RenameLabels(string fileLocation)
+        {
+            try
+            {
+                var oldText = File.ReadAllText(fileLocation);
+                var newText = replacements.Aggregate(oldText, (current, replacement) => current.Replace(replacement.Key, replacement.Value));
+                //Equivalent to
+                //foreach (string toReplace in replacements.Keys)
+                //{
+                //    oldText = s.Replace(toReplace, Replacements[toReplace]);
+                //}
+                //return s;
+
+                File.WriteAllText(fileLocation, newText);
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
 
- 
-
-        private void btnRemoveSelected_Click(object sender, EventArgs e)
+        private void RemoveDuplicates(string fileLocation)
         {
-            lstLabelTracks.Items.Remove(lstLabelTracks.SelectedItem);
+            try
+            {
+                string[] lines = File.ReadAllLines(fileLocation);
+                File.WriteAllLines(fileLocation, lines.Distinct().ToArray());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
     }
 }
